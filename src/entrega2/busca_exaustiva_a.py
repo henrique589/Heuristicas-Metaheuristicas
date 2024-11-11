1f37773f9e141c6b56376f9faaa6ffe811089ce3
Henrique Azevedo <henriqueazevedoandrade@gmail.com>
2024-11-11 10:36:46 -0300

    :pencil: busca_exaustiva_a

--- src/entrega2/busca_exaustiva_a.py
+++ src/entrega2/busca_exaustiva_a.py
@@ -1,15 +1,25 @@
-import itertools
+import time
 
-def generate_permutations(n):
-    # Cria uma lista com os números de 1 a n
-    numbers = list(range(1, n + 1))
-    # Gera todas as permutações
-    perms = itertools.permutations(numbers)
-    
-    # Itera e imprime cada permutação
-    for perm in perms:
-        print(perm)
+def permutacao(nums: list[int]):
+    n = len(nums)
+    perm, solucao = [], []
 
-# Exemplo de uso:
-n = int(input("Digite o valor de n: "))
-generate_permutations(n)
+    def backtrack():
+        if len(solucao) == n:
+            print(solucao)
+            return
+        
+        for x in nums:
+            if x not in solucao:
+                solucao.append(x)
+                backtrack()
+                solucao.pop()
+
+    start_time = time.time()
+    backtrack()
+    end_time = time.time()
+    print(f'Tempo de processamento = {end_time - start_time:.4f} segundos')
+
+n = int(input('Digite um número n: '))
+nums = list(range(1, n + 1))
+permutacao(nums)
